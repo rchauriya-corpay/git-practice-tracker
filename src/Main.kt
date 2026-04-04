ed16574c0a0bceccce4993866e73cc401f52c0d1
Rajat <145740836+rchauriya-corpay@users.noreply.github.com>
2026-04-04 14:01:45 +0530

    rajat final (#2)

--- src/Main.kt
+++ src/Main.kt
@@ -3,6 +3,10 @@ fun main() {
     val tasks = mutableListOf<String>()
 
     tasks.addAll(0, listOf("Add unit tests", "Add"))
+    tasks.add(1, "Add more features")
+    tasks.add(2, "Optimize performance")
+    tasks.add(3, "Fix bugs")
+
 
     println("Current Tasks:")
     if (tasks.isEmpty()) {
